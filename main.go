f959aa5cabd7e80d4e5241a07114771b3457c12d
Gabriel Porto <gabrielportol@hotmail.com>
2023-09-02 16:02:03 -0300

    feat: a página agora é carregada dinamicamente e adicionado git ignore

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"text/template"
+)
+
+type Produto struct {
+	Nome, Descricao string
+	Preco           float64
+	Quantidade      int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":4200", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{{"Camiseta", "Azul, bem bonita", 39., 1},
+		{"Tenis", "Confortável", 89., 3},
+		{"Fone", "Para orelha", 100.99, 1}}
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
